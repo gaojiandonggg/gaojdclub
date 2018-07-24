021756f46e5631439611e2975993950394db7599
skydemo <skydemo@126.com>
2018-07-24 10:54:24 +0800

    4543

--- SignalRDemo/Controllers/HomeController.cs
+++ SignalRDemo/Controllers/HomeController.cs
@@ -20,6 +20,7 @@ namespace SignalRDemo.Controllers
             ViewData["Message"] = "Your application description page.";
 
             return View();
+
         }
 
         public IActionResult Contact()
